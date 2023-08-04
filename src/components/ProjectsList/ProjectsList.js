414499b970fb1fd2a80f4440b7c5ed741df35a04
Ulisses <ulissesroque@protonmail.com>
2023-08-04 03:26:44 -0300

    feat: implement ProjectsList component

--- src/components/ProjectsList/ProjectsList.js
+++ src/components/ProjectsList/ProjectsList.js
@@ -1,17 +1,54 @@
-import Card from 'react-bootstrap/Card';
-function ProjectBox(project) {
+import './ProjectsList.css';
+
+function ProjectsList() {
+    // const track = document.getElementById('image-track')
+
+    // if (track.dataset.mouseDownAt === '0') return;
+      
+    // window.onmousedown = e => {
+    //   track.dataset.mouseDownAt = e.clientX;
+    // }
+
+    // window.onmousemove = e => {
+    //   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    //     maxDelta = window.innerWidth / 2;
+      
+    //   const percentage = (mouseDelta / maxDelta) * -100,
+    //   nextPercentage = parseFloat(track.dataset.pervPercentage + percentage);
+    //   window.dataset.percentage = nextPercentage;
+
+    //   track.animate({
+    //     transform: `translate(${nextPercentage}%, -50%)`
+    //   }, { duration: 1200, fill: 'forwards' });
+
+    //   for (const image of track.getElementsByClassName('project-image')) {
+    //     image.animate({
+    //       objectPosition: `${nextPercentage + 100}% center`
+    //   }, { duration: 1200, fill: 'forwards' });
+    //   }
+    // }
+    // window.onmouseup = () => {
+    //   track.dataset.mouseDownAt = '0';
+    //   track.dataset.pervPercentage = track.dataset.percentage;
+    // }
+
   return (
-    <li>
-      <a href={project.link}>
-        <Card className="text-center">
-          <Card.Img variant="top" src={`${project.image}/100px180?text=${project.name}`} />
-          <Card.Body>
-            <Card.Title>{project.name}</Card.Title>
-          </Card.Body>
-        </Card>
-      </a>
-    </li>
-  );
+    <main>
+      <div id='image-track' data-mouse-down-at='0' data-prev-percentage='0'>
+        <img className='project-image' src='https://images.unsplash.com/photo-1690983177757-9ef05b356694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80' draggable='false'></img>
+
+        <img className='project-image' src='https://images.unsplash.com/photo-1690983177757-9ef05b356694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80' draggable='false'></img>
+
+        <img className='project-image' src='https://images.unsplash.com/photo-1690983177757-9ef05b356694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80' draggable='false'></img>
+
+        <img className='project-image' src='https://images.unsplash.com/photo-1690983177757-9ef05b356694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80' draggable='false'></img>
+
+        <img className='project-image' src='https://images.unsplash.com/photo-1690983177757-9ef05b356694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80' draggable='false'></img>
+
+        <img className='project-image' src='https://images.unsplash.com/photo-1690983177757-9ef05b356694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80' draggable='false'></img>
+      </div>
+    </main>
+  )
 }
 
-export default ProjectBox;
+export default ProjectsList;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // import HamburguerMenu from '../components/HamburguerMenu';
 // import Header from '../components/Header';
+import ProjectsList from '../components/ProjectsList/ProjectsList'
 import Footer from '../components/Footer';
 
 function Projects() {
   return (
     <div className="Projects">
+      <ProjectsList />
       <Footer />
     </div>
   );
